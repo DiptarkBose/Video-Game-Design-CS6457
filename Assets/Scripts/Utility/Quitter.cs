507a75112836cda6a295ca9b23940fa7635a8cc6
Jeff Wilson <jeff@imtc.gatech.edu>
2021-05-21 15:29:23 -0400

    switched to windowed build mode, added ESC quit, and lighting adjustment

---
+++ Assets/Scripts/Utility/Quitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            QuitGame();
+        
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
